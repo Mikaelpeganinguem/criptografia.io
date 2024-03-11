2d3f2554b9b3a390141aae53b124eac4b519672b
Mikael Carlos <139816735+Mikaelpeganinguem@users.noreply.github.com>
2024-03-11 17:49:45 -0300

    Atualizar o main.js

--- assets/code/cesar/main.js
+++ assets/code/cesar/main.js
@@ -1,4 +1,5 @@
 import {criptografia} from "./encryptCesar.js";
+import {descriptografiaDeCesar} from "./decryptCesar.js";
 
 document.getElementById("encryptCes").addEventListener("click",function(event) {
     let mensagem = document.getElementById("mensagem").value.toLowerCase();
@@ -6,6 +7,16 @@ document.getElementById("encryptCes").addEventListener("click",function(event) {
         
     let encrypt = criptografia(mensagem, deslocamento);
     document.getElementById("result").innerText = encrypt;
+});
+
+
+document.getElementById("decryptCes").addEventListener("click", function(event){
+    let mensagem = document.getElementById("mensagem").value.toLowerCase();
+    
+    let deslocamento = parseInt(document.getElementById("desloc").value);
+    let decrypt = descriptografiaDeCesar(mensagem, deslocamento);
+    document.getElementById("result").innerText = decrypt
+    
 });
     
 document.getElementById("toClean").addEventListener("click", function(event) {
